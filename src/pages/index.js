638ed7959d3910b45741c2db7e60706f48b0da00
MartinHerssens <s117589@ap.be>
2020-12-03 14:24:36 +0100

    add gastby-node

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,47 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 
-const IndexPage = () => (
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+  wpcontent {
+    page(id: "home", idType: URI) {
+      homePageMeta {
+        homePageDescription
+        homePageTitle
+        homePagePicture {
+          sourceUrl
+          imageFile {
+            childImageSharp {
+              fluid(quality:100){
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+        homePageFeaturedCars {
+          ... on WPGraphql_Car {
+            id
+            car {
+              brand
+              model
+              pk
+            }
+          }
+        }
+      }
+    }
+  }
+}
+  `);
+  return (
   <Layout>
     <SEO title="Home" />
 
   </Layout>
-)
+  )}
 
 export default IndexPage
