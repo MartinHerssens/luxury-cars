5b7359238fd4b715488618a4f89e0677248d93c3
MartinHerssens <s117589@ap.be>
2020-12-03 19:55:18 +0100

    build dynamic pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,42 @@
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
+const {graphql}= require("gatsby")
+const path = require("path")
+// dynamische pagina's creëren
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      wpcontent {
+        cars {
+          edges {
+            node {
+              slug
+              id
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      result.errors.forEach(e => console.error(e.toString()))
+      return Promise.reject(result.errors)
+    }
 
+    const cars = result.data.wpcontent.cars.edges
+    cars.forEach(car => {
+      const { id, slug } = car.node
+      createPage({
+        path: slug,
+        component: path.resolve(`src/templates/car.js`),
+        context: {
+          id,
+          slug,
+        },
+      })
+    })
+  })
+}
 /* Aan de hand van dit stukje code worden de images vanuit WPgraphql omgezet tot images waarop Gatsby image optimization kan toepassen */
 exports.createResolvers = async ({
   actions,
